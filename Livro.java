6b9f82a09f04eb4c1f6aba51e104d81e1aa32626
Nara Alencar <116104559+naracodes@users.noreply.github.com>
2023-01-20 21:14:04 -0300

    Create Livro.java

---
+++ Livro.java
@@ -0,0 +1,37 @@
+package br.com.ada.polotech.livraria.modelos;
+
+public class Livro extends Produto {
+    private String escritor, editora;
+
+    public static int quantidadeDeLivros = 0;
+
+    public Livro(String nomeDoProduto, Double precoDoProduto, String escritor, String editora) {
+        super(nomeDoProduto, precoDoProduto);
+        this.escritor = escritor;
+        this.editora = editora;
+    }
+    public String getEscritor() {
+        return escritor;
+    }
+
+    public void setEscritor(String escritor) {
+        this.escritor = escritor;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                super.toString() +
+                " escritor: '" + escritor + '\'' +
+                ", editora: '" + editora + '\'' +
+                '}';
+    }
+}
